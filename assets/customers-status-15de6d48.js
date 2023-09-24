25f71dcda35ab030cb1f4eb500dbd77d47ed4df2
cn00163173 <cn00163173@126.com>
2023-09-24 12:45:12 +0800

    deploy

---
+++ assets/customers-status-15de6d48.js
@@ -0,0 +1,1 @@
+const t=[{value:1,text:"已关联"},{value:2,text:"考察中"}];export{t as s};
---
+++ assets/groups-581ef8ff.js
@@ -0,0 +1,1 @@
+const i=[{id:"1",name:"技术部",description:"--"},{id:"2",name:"测试部",description:"--"}];export{i as g};
