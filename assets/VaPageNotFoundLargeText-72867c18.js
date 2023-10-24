d480e028e29f17d5198cf03e3b433062d25581ee
cn00163173 <cn00163173@126.com>
2023-10-24 10:14:55 +0800

    deploy

--- assets/VaPageNotFoundLargeText-72867c18.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./VaPageNotFound.vue_vue_type_style_index_0_lang-ff78d4a1.js";import{f as a,l as t,w as o,o as _,a as n}from"./index-f70b0475.js";const s=n("div",{class:"va-page-not-found-large-text__number"},"404",-1),g=a({__name:"VaPageNotFoundLargeText",setup(r){return(c,p)=>(_(),t(e,{class:"va-page-not-found-large-text"},{image:o(()=>[s]),_:1}))}});export{g as default};
--- assets/customers-type-2dfa0339.js
+++
@@ -1,1 +0,0 @@
-const t=[{value:1,text:"自持客户"},{value:2,text:"线索"}],e=[{value:1,text:"移动云客户"},{value:2,text:"自持客户"}];export{t as s,e as t};
