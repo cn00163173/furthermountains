1a33a0a31c62d920d41a366067b117bbb2c3cc88
cn00163173 <cn00163173@126.com>
2023-11-07 15:28:08 +0800

    deploy

---
+++ assets/roles-012a2e71.js
@@ -0,0 +1,1 @@
+const e=[{id:"1",name:"销售",type:1,member:10,description:"经销商销售角色"},{id:"2",name:"测试角色",type:2,member:10,description:"-"}];export{e as r};
---
+++ assets/roles-a8c5a26c.js
@@ -0,0 +1,1 @@
+const n=[{value:1,text:"系统预设角色"},{value:2,text:"自定义角色"}];function o(e){for(const t of n)if(t.value==e)return t.text;return""}export{o as g};
