c20124001be3e5066e33d4569e27b4e091fbddff
cn00163173 <cn00163173@126.com>
2023-11-09 15:17:25 +0800

    deploy

--- assets/customers-source-243249eb.js
+++
@@ -1,1 +0,0 @@
-const t=[{value:1,text:"代理"},{value:2,text:"代售"}],e=[{value:1,text:"自持客户"},{value:2,text:"线索"}];export{e as s,t};
--- assets/partners-type-b0aff8a6.js
+++
@@ -1,1 +0,0 @@
-const t=[{id:"1",name:"见山科技（苏州）有限公司",type:1,description:"-"},{id:"2",name:"浪潮集团有限公司",type:2,description:"-"}],e=[{value:1,text:"经销商"},{value:2,text:"代理商"}];export{t as p,e as t};
